3b0b92ddb15ddd5be3e63688886df4f4ba606580
Matheus Ribeiro <mathribe2020@gmail.com>
2022-09-17 02:07:35 -0300

    feat: [server] - prisma initial configurations and database migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,28 @@
-import express from 'express';
+import express, { application } from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+	return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+	return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+	const gameId = req.params.id;
 	res.json([
 		{ name: 'Google', url: 'https://google.com' },
 		{ name: 'Facebook', url: 'https://facebook.com' },
 		{ name: 'Twitter', url: 'https://twitter.com' },
 		{ name: 'Instagram', url: 'https://instagram.com' },
 	]);
+});
+
+app.get('ads/:id/discord', (req, res) => {
+	const adId = req.params.id;
+	return res.json([]);
 });
 
 app.listen(3333, () => {
